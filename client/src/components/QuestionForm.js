19fd6828fd31f5b697f75ef726c67c91b649b343
aboozarmojdeh <aboozar.mojdeh@gmail.com>
2020-12-31 13:29:49 -0800

    client side show question form in progress

--- client/src/components/QuestionForm.js
+++ client/src/components/QuestionForm.js
@@ -1,31 +1,30 @@
-import React, { useState, useContext,useEffect } from "react";
-import AnswerQuestion from '../apis/AnswerQuestion';
-
+import React, { useState, useContext, useEffect } from "react";
+import AnswerQuestion from "../apis/AnswerQuestion";
+// import ShowForm from './ShowForm';
 const QuestionForm = () => {
-    // const { id } = useParams();
-    // let history=useHistory();
+  // const { id } = useParams();
+  // let history=useHistory();
   const [questions, setQuestions] = useState([]);
   const [answer, setAnswer] = useState("");
- 
-const [labelSelected,SetLabelSelected]=useState("");
-const [idSelected,SetIdSelected]=useState("");
+  const [labelSelected, SetLabelSelected] = useState("");
+  const [idSelected, SetIdSelected] = useState("");
 
+  const [isShowForm,setIsShowForm]=useState(false);
 
   useEffect(() => {
     const questionGrabber = async () => {
       const response = await AnswerQuestion.get("/");
-    //   const questions = await response.json();
-      console.log('questions',response.data);
+      //   const questions = await response.json();
+      console.log("questions", response.data);
       setQuestions(response.data);
-      let randomNumber=getRandomIntExclusive(0, response.data.length - 1);
-      console.log('randomNumber',randomNumber)
+      let randomNumber = getRandomIntExclusive(0, response.data.length - 1);
+      console.log("randomNumber", randomNumber);
       SetLabelSelected(response.data[randomNumber].question_text);
-    SetIdSelected(response.data[randomNumber].question_id)
-    console.log('id',response.data[randomNumber].question_id)
-
+      SetIdSelected(response.data[randomNumber].question_id);
+      console.log("id", response.data[randomNumber].question_id);
     };
     questionGrabber();
-    
+
     // let randomQuestionGenerator=questions[getRandomIntInclusive(0, questions.length - 1)];
     // console.log('randomQuestion',randomQuestion)
     // setRandomQuestion(randomQuestionGenerator)
@@ -38,12 +37,13 @@ const [idSelected,SetIdSelected]=useState("");
 
   const handleSubmitAnswer = async (e) => {
     e.preventDefault();
-   
+
     const updateQuestion = await AnswerQuestion.put(`/${idSelected}`, {
-        question_text:labelSelected,
-        answer_text: answer
+      question_text: labelSelected,
+      answer_text: answer,
     });
     // history.push('/dashboard');
+    setIsShowForm(false)
   };
 
   const getRandomIntExclusive = (min, max) => {
@@ -51,19 +51,47 @@ const [idSelected,SetIdSelected]=useState("");
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
   };
+/// show form
+// const showFormComponent = () => {
+//   return (
+//     <div> 
+//    <form id= "add-app">
+//    <div className="input-group mb-3">
+//           {questions.length ? (
+//             <label htmlFor="name">{labelSelected}</label>
+//           ) : (
+//             <div>Loading Question</div>
+//           )}
+//           <input
+//             value={answer}
+//             onChange={nameChange}
+//             id="name"
+//             className="form-control"
+//             type="text"
+//           />
+//         </div>
+//         <button
+//           type="submit"
+//           onClick={handleSubmitAnswer}
+//           className="btn btn-primary"
+//         >
+//           Submit
+//         </button>
+//      </form>
+//      </div>
+//     );
+// };
 
-  
-  
+///
   return (
-    <div>
-      <form>
-        <div className="input-group mb-3">
+    <div className="container my-3">
+      <button type="button" class="btn btn-primary" onClick={() => setIsShowForm(true) }>
+        Ask me!
+      </button>
+      {isShowForm ? (<form id= "add-app">
+   <div className="input-group mb-3">
           {questions.length ? (
-            <label htmlFor="name">
-              {
-               labelSelected
-              }
-            </label>
+            <label htmlFor="name">{labelSelected}</label>
           ) : (
             <div>Loading Question</div>
           )}
@@ -82,7 +110,7 @@ const [idSelected,SetIdSelected]=useState("");
         >
           Submit
         </button>
-      </form>
+     </form>) : null}
     </div>
   );
 };
